84e49849e354953576caff5793c090d5d4cc7202
afonsomiguel <afonso.miguel@gmail.com>
2025-05-18 18:26:54 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use std::thread::sleep;
+use std::time::Duration;
+
+fn create_bars(num_bars: u64, total_steps: u64) -> Vec<ProgressBar> {
+    let multi_progress = MultiProgress::new();
+    let mut bars = Vec::with_capacity(num_bars as usize);
+
+    for i in 0..num_bars {
+        let bar = multi_progress.add(ProgressBar::new(total_steps));
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template(&format!("Task {}: [{{bar:30}}] {{pos}}/{{len}} {{msg}}", i + 1))
+                .expect("Failed to set progress style.")
+                .progress_chars("=> "),
+        );
+        bars.push(bar);
+    }
+
+    bars
+}
+
+fn main() {
+    const STEPS_PER_TASK: u64 = 60;
+    let bars = create_bars(2, STEPS_PER_TASK);
+
+    for i in 0..STEPS_PER_TASK {
+        sleep(Duration::from_millis(100));
+        bars[0].inc(1);
+        bars[1].set_position(STEPS_PER_TASK - i);
+    }
+}
