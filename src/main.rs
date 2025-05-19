663e04d2e024df2effc4568c396ceadfec4e6656
afonsomiguel <afonso.miguel@gmail.com>
2025-05-19 09:27:06 -0300

    Ajustes

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use std::thread::sleep;
 use std::time::Duration;
 
-fn create_bars(multi_progress: &MultiProgress, num_bars: u64, total_steps: u64) -> Vec<ProgressBar> {
+fn create_bars(mp: &MultiProgress, num_bars: u64, total_steps: u64) -> Vec<ProgressBar> {
     let mut bars = Vec::with_capacity(num_bars as usize);
 
     for i in 0..num_bars {
-        let bar = multi_progress.add(ProgressBar::new(total_steps));
+        let bar = mp.add(ProgressBar::new(total_steps));
         bar.set_style(
             ProgressStyle::default_bar()
                 .template(&format!("Task {}: [{{bar:30}}] {{pos}}/{{len}} {{msg}}", i + 1))
