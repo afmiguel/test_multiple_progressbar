8d504e461ca05186bd2fb338edffaaba59858d32
afonsomiguel <afonso.miguel@gmail.com>
2025-05-19 11:51:43 -0300

    Refatorando com Threads

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,39 @@ use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use std::thread::sleep;
 use std::time::Duration;
 
-fn create_bars(mp: &MultiProgress, num_bars: u64, total_steps: u64) -> Vec<ProgressBar> {
-    let mut bars = Vec::with_capacity(num_bars as usize);
+const STEPS_PER_TASK: u64 = 10;
 
-    for i in 0..num_bars {
-        let bar = mp.add(ProgressBar::new(total_steps));
+fn main() {
+    let multi_progress = MultiProgress::new();
+    let mut handles = vec![];
+
+    for thread_index in 0..2 {
+        let bar = multi_progress.add(ProgressBar::new(STEPS_PER_TASK));
+        // Formata a barra de progresso
         bar.set_style(
             ProgressStyle::default_bar()
-                .template(&format!("Task {}: [{{bar:30}}] {{pos}}/{{len}} {{msg}}", i + 1))
+                .template(&format!("Task {}: [{{bar:30}}] {{pos}}/{{len}} {{msg}}", thread_index + 1))
                 .expect("Failed to set progress style.")
                 .progress_chars("=> "),
         );
-        bars.push(bar);
+
+        // Dispara a Thread
+        let h = std::thread::spawn(move || {
+            for _ in 0..STEPS_PER_TASK {
+
+                //////////// Faz alguma coisa
+                sleep(Duration::from_millis((thread_index+1)*100)); // A pausa depende do índice da thread
+                ////////////
+
+                bar.inc(1); // Incrementa a barra de progresso (considerar o limite máximo disposto em ProgressBar::new(X))
+            }
+            bar.finish_with_message("Done!"); // Finaliza a barra de progresso
+        });
+        handles.push(h);
     }
 
-    bars
-}
-
-fn main() {
-    const STEPS_PER_TASK: u64 = 60;
-
-    let multi_progress = MultiProgress::new();
-    let bars = create_bars(&multi_progress, 2, STEPS_PER_TASK);
-
-    for i in 0..STEPS_PER_TASK {
-        sleep(Duration::from_millis(100));
-        bars[0].inc(1);
-        bars[1].set_position(STEPS_PER_TASK - i);
+    // Aguarda as Threads terminarem
+    for h in handles {
+        h.join().unwrap();
     }
 }
